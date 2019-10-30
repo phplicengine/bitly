d605fa70f873cb55c27d063e4709c8a14a5825fc
PHPLicengine <phplicengine@gmail.com>
2019-10-30 23:06:18 +0330

    Create Campaign.php

---
+++ lib/PHPLicengine/Service/Campaign.php
@@ -0,0 +1,110 @@
+<?php
+
+// Campaign.php
+#################################################
+##
+## PHPLicengine
+##
+#################################################
+## Copyright 2009-{current_year} PHPLicengine
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+#################################################
+
+namespace PHPLicengine\Service;
+use PHPLicengine\Exception\ResponseException;
+use PHPLicengine\Exception\CurlException;
+
+class Campaign extends \PHPLicengine\Api\Api {
+ 
+      private $url;
+      
+      public function __construct ($api_key)
+      {
+             parent::__construct($api_key);
+             $this->url = 'https://api-ssl.bitly.com/v4';       
+
+      }
+ 
+      /*
+      Create Channel
+      https://dev.bitly.com/v4/#operation/createChannel
+      */
+      public function createChannel(array $params)
+      {
+             return $this->post($this->url . '/channels', $params);
+      }
+
+      /*
+      Retrieve Channels
+      https://dev.bitly.com/v4/#operation/getChannels
+      */
+      public function getChannels(array $params)
+      {
+             return $this->get($this->url . '/channels', $params);
+      }
+      
+      /*
+      Create Campaign
+      https://dev.bitly.com/v4/#operation/createCampaign
+      */
+      public function createCampaign(array $params)
+      {
+             return $this->post($this->url . '/campaigns', $params);
+      }
+      
+      /*
+      Retrieve Campaigns
+      https://dev.bitly.com/v4/#operation/getCampaigns
+      */
+      public function getCampaigns(array $params)
+      {
+             return $this->get($this->url . '/campaigns', $params);
+      }
+      
+      /*
+      Retrieve a Campaign
+      https://dev.bitly.com/v4/#operation/getCampaign
+      */
+      public function getCampaign(string $campaign_guid)
+      {
+             return $this->get($this->url . '/campaigns/'.$campaign_guid);
+      }
+      
+      /*
+      Update Campaign
+      https://dev.bitly.com/v4/#operation/updateCampaign
+      */
+      public function updateCampaign(string $campaign_guid, array $params)
+      {
+             return $this->patch($this->url . '/campaigns/'.$campaign_guid, $params);
+      }
+      
+      /*
+      Get A Channel
+      https://dev.bitly.com/v4/#operation/getChannel
+      */
+      public function getChannel(string $channel_guid)
+      {
+             return $this->get($this->url . '/channels/'.$channel_guid);
+      }
+      
+      /*
+      Update A Channel
+      https://dev.bitly.com/v4/#operation/updateChannel
+      */
+      public function updateChannel(string $channel_guid, array $params)
+      {
+             return $this->patch($this->url . '/channels/'.$channel_guid, $params);
+      }      
+}
