87ab6a5c7472e01ea98b4beffaf7c259dcb50973
PHPLicengine <phplicengine@gmail.com>
2021-01-01 18:04:52 +0330

    Create WebhookTest.php

---
+++ lib/PHPLicengine/tests/WebhookTest.php
@@ -0,0 +1,99 @@
+<?php
+
+// WebhookTest.php
+#################################################
+##
+## PHPLicengine
+##
+#################################################
+## Copyright 2009-{current_year} PHPLicengine
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+#################################################
+
+use PHPLicengine\Api\ApiInterface;
+use PHPLicengine\Service\Webhook;
+use PHPUnit\Framework\TestCase;
+
+class WebhookTest extends TestCase
+{
+    
+    public function testCreateWebhook()
+    {
+        $mock = $this->createMock(ApiInterface::class);
+        $mock
+            ->expects($this->once())
+            ->method('post')
+            ->with(
+                    $this->equalTo('https://api-ssl.bitly.com/v4/webhooks'),
+                    $this->identicalTo(['key' => 'value'])
+                    );
+        $bitlink = new Webhook($mock);
+        $bitlink->createWebhook(['key' => 'value']);
+    } 
+
+    public function testGetWebhooks()
+    {
+        $mock = $this->createMock(ApiInterface::class);
+        $mock
+            ->expects($this->once())
+            ->method('get')
+            ->with(
+                    $this->equalTo('https://api-ssl.bitly.com/v4/organizations/test/webhooks')
+                  );
+        $bitlink = new Webhook($mock);
+        $bitlink->getWebhooks('test');
+    } 
+
+    public function testGetWebhook()
+    {
+        $mock = $this->createMock(ApiInterface::class);
+        $mock
+            ->expects($this->once())
+            ->method('get')
+            ->with(
+                    $this->equalTo('https://api-ssl.bitly.com/v4/webhooks/test')
+                  );
+        $bitlink = new Webhook($mock);
+        $bitlink->getWebhook('test');
+    } 
+
+    public function testUpdateWebhook()
+    {
+        $mock = $this->createMock(ApiInterface::class);
+        $mock
+            ->expects($this->once())
+            ->method('patch')
+            ->with(
+                    $this->equalTo('https://api-ssl.bitly.com/v4/webhooks/test'),
+                    $this->identicalTo(['key' => 'value'])
+
+                  );
+        $bitlink = new Webhook($mock);
+        $bitlink->updateWebhook('test', ['key' => 'value']);
+    } 
+
+    public function testDeleteWebhook()
+    {
+        $mock = $this->createMock(ApiInterface::class);
+        $mock
+            ->expects($this->once())
+            ->method('delete')
+            ->with(
+                    $this->equalTo('https://api-ssl.bitly.com/v4/webhooks/test')
+                  );
+        $bitlink = new Webhook($mock);
+        $bitlink->deleteWebhook('test');
+    } 
+
+}
