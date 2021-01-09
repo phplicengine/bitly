e7d2113e5ba38360fa1940a641c711bf6109f7e9
PHPLicengine <phplicengine@gmail.com>
2021-01-09 18:21:50 +0330

    Create OAuthTest.php

---
+++ lib/PHPLicengine/tests/OAuthTest.php
@@ -0,0 +1,45 @@
+<?php
+
+// OAuthTest.php
+#################################################
+##
+## PHPLicengine
+##
+#################################################
+## Copyright 2009-{current_year} PHPLicengine
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+#################################################
+
+use PHPLicengine\Api\ApiInterface;
+use PHPLicengine\Service\OAuth;
+use PHPUnit\Framework\TestCase;
+
+class OAuthTest extends TestCase
+{
+
+    public function testGetOAuthToken()
+    {
+        $mock = $this->createMock(ApiInterface::class);
+        $mock
+            ->expects($this->once())
+            ->method('post')
+            ->with(
+                    $this->equalTo('https://api-ssl.bitly.com/oauth/access_token'),      
+                    $this->identicalTo(['key' => 'value'])
+                    );
+        $oauth = new OAuth($mock);
+        $oauth->getOAuthToken(['key' => 'value']);
+    }        
+
+}
